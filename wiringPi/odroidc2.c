8bb1053f0ff37b0154b11eba92b8b59a2c1b8867
Deokgyu Yang <secugyu@gmail.com>
2021-05-10 12:25:53 +0900

    WiringPi: Fix checking if gpiomem exists or not
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>
    Change-Id: I828feaf6115d01dd038e1da08bc92ff7f82b7787

--- wiringPi/odroidc2.c
+++ wiringPi/odroidc2.c
@@ -696,7 +696,12 @@ static void init_gpio_mmap (struct libodroid *libwiring)
 				"wiringPiSetup: Unable to open /dev/mem: %s\n",
 				strerror (errno));
 	} else {
-		if (access("/dev/gpiomem",0) == 0 && (fd = open ("/dev/gpiomem", O_RDWR | O_SYNC | O_CLOEXEC) ) < 0) {
+		if (access("/dev/gpiomem",0) == 0) {
+			if ((fd = open ("/dev/gpiomem", O_RDWR | O_SYNC | O_CLOEXEC)) < 0) {
+				msg(MSG_ERR,
+					"wiringPiSetup: Unable to open /dev/gpiomem: %s\n",
+					strerror (errno));
+			}
 			setUsingGpiomem(TRUE);
 		} else if (isGpiodInstalled()) {
 			initGpiod(libwiring);
